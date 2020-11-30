f0c5bbf794457e8243f21f764fae40bda99b5df4
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2020-11-30 18:24:23 +0530

    Start of Python conversion
    Added class 'match' with constructor and needed variables.

---
+++ new_code.py
@@ -0,0 +1,23 @@
+class match:
+    score = {"User": [0, 0], "CPU": [0, 0]}
+    played = 0
+    isSecInn = False
+
+    def __play(self):
+        
+        
+    def __init__(self, pages, overs, wickets, pBat):
+        self.pages = pages
+        self.balls = overs * 6
+        self.wickets = wickets
+        self.pBat = pBat
+
+        self.play()
+
+
+    
+        
+
+
+        
+
